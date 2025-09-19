8e25a1d95dcfd9423a0b7b92ada7ab5ee805564a
tseevag <shivagaihre40@gmail.com>
2025-09-19 17:10:46 +0545

    fixed test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var mathFns = require('./math-fn')
 
 const n1 = Number(prompt('Enter N1: '))
 const n2 = Number(prompt('Enter N2: '))
-''
+
 console.log(`Sum: ${mathFns.sum(n1, n2)}`)
 console.log(`Difference: ${mathFns.sub(n1, n2)}`)
 console.log(`Multiplication: ${mathFns.mul(n1, n2)}`)
